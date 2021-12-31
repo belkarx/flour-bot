7a800e087ff6ae8c44d86faedb44f1730be2d306
belkarx <84473367+belkarx@users.noreply.github.com>
2021-12-30 19:10:48 -0800

    Added minecraft status because why not

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import discord
 from discord.utils import get
 from discord.ext import commands
+from mcstatus import MinecraftServer
+
 
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
@@ -29,7 +31,15 @@ async def on_message(msg):
         await msg.add_reaction(emoji=sexy_emoji)
 
     if msg.content.startswith("$"):
-        #changes command arg to leetspeak
+        cmd = msg.content
+        if cmd.startswith("$status"):
+            try:
+                status = MinecraftServer.lookup(cmd[8:]).status()
+                await msg.channel.send(f"The server has {status.players.online} players and replied in {status.latency} ms")
+            except:
+                await msg.channel.send("invalid address or argument. Usage: $status [mc server name[:port]]")
+
+                    #changes command arg to leetspeak
         if msg.content.startswith("$leet"):
             await msg.channel.send(msg.content[5:].replace("A","4").replace("a","@").replace("B","8").replace("b","6").replace("C", "[").replace("E", "3").replace("g", "9").replace("I", "|").replace("J", "]").replace("O", "0").replace("T","7").replace("l", "1").replace("S", "$").replace("s", "5").replace("t", "+").replace("Z", "2"))
 
